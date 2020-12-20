a6de2a317fa99e051f8780bee704895c56fe7576
Ubuntu <ubuntu@ip-172-16-1-5.eu-west-1.compute.internal>
2020-12-20 15:26:36 +0000

    install vim plug

--- install.sh
+++ install.sh
@@ -3,5 +3,11 @@
 sudo apt-get update -y
 sudo apt-get install -y neovim
 
+# install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 mkdir -p ~/.config/nvim
 ln -sf "$(pwd)/init.vim" ~/.config/nvim/init.vim
+
+# Now open nvim, and run :PlugInstall
